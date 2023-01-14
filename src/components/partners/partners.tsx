c109eb059a0db92c2ca79141598d4c6d03aba7ba
Вячеслав <pudra@live.ru>
2023-01-14 21:29:46 +0500

    Modify patners block highlight

--- src/components/partners/partners.tsx
+++ src/components/partners/partners.tsx
@@ -5,25 +5,27 @@ function Partners():JSX.Element {
   return (
     <section className='partners'>
       <div className='container'>
-        <div className='partners__inner'>
-          <h2 className='visually-hidden'>Our partners</h2>
-          <p className='partners__text'>Trusted more than 150+ brand</p>
-          <ul className='partners__list'>
-            {partnerList.map((item) => (
-              <li className='partners__list-item' key={item.name}>
-                <a
-                  className={`partners__item-link partners__item-link--${item.name.toLowerCase()}`}
-                  href={item.url} target='_blank'
-                  rel='noopener noreferrer'
-                  aria-label={item.name}
-                >
-                  <svg>
-                    <use xlinkHref={`img/sprite/sprite.svg#${item.name.toLowerCase()}`}></use>
-                  </svg>
-                </a>
-              </li>)
-            )}
-          </ul>
+        <div className='partners__highlight'>
+          <div className='partners__inner'>
+            <h2 className='visually-hidden'>Our partners</h2>
+            <p className='partners__text'>Trusted more than 150+ brand</p>
+            <ul className='partners__list'>
+              {partnerList.map((item) => (
+                <li className='partners__list-item' key={item.name}>
+                  <a
+                    className='partners__item-link'
+                    href={item.url} target='_blank'
+                    rel='noopener noreferrer'
+                    aria-label={item.name}
+                  >
+                    <svg width='194' height='51'>
+                      <use xlinkHref={`img/sprite/sprite.svg#${item.name.toLowerCase()}`}></use>
+                    </svg>
+                  </a>
+                </li>)
+              )}
+            </ul>
+          </div>
         </div>
       </div>
     </section>
